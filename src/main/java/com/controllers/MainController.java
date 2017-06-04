091c416c28def0b586843739490d4fcfc7ff87cc
Louis Tan-Therrien <louis.tant@gmail.com>
2017-06-03 21:29:43 -0400

    Log with slf4j

---
+++ src/main/java/com/controllers/MainController.java
@@ -0,0 +1,24 @@
+package com.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+  @RequestMapping("/")
+  public String index() {
+
+      final Logger log = LoggerFactory.getLogger(this.getClass());
+
+      // Log a simple message
+      log.debug("debug level log");
+      log.info("info level log");
+      log.error("error level log");
+
+      return "hello.html";
+  }
+
+}
